7de7db769c9c2b25cbec8d96677c94c9fef58371
abdulmalishakir2001 <abdulmalikshakir2001@gmail.com>
2024-01-28 11:04:47 +0500

    adding .env file

--- src/components/ImageGallerySlider.tsx
+++ src/components/ImageGallerySlider.tsx
@@ -23,7 +23,7 @@ const ImageGallerySlider = () => {
   const [cards,setCards] = useState<ICardsType[]> ([])
   const [runUseEffectAgain,setRunUseEffectAgain] = useState(false)
   useEffect(()=>{
-    fetchApi('/api/project/allProjects',"GET",{}).then((data)=>{
+    fetchApi(`${process.env.BACKEND_URL}/api/project/allProjects`,"GET",{}).then((data)=>{
       setCards(data.allProjects)
     })
     setRunUseEffectAgain(true)
