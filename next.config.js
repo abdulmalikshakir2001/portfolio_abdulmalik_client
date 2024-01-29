7824e7bd9366bd62cf882334f308c6a55ce226cc
abdulmalishakir2001 <abdulmalikshakir2001@gmail.com>
2024-01-29 20:09:54 +0500

    fix image gallery slider hover on title

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/:path*',
-        destination: 'https://portfolio-abdulmalik-server.vercel.app/:path*', // Proxy to Backend
+        destination: 'http://localhost:8000/:path*', // Proxy to Backend
       },
     ];
   },
--- src/components/ImageGallerySlider.tsx
+++ src/components/ImageGallerySlider.tsx
@@ -1,8 +1,8 @@
-'use client'
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
-import { FaAnglesRight,FaAnglesLeft } from "react-icons/fa6";
-import { fetchApi } from '@/utility_functions/fetchApi';
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { FaAnglesRight, FaAnglesLeft } from "react-icons/fa6";
+import { fetchApi } from "@/utility_functions/fetchApi";
 interface ICardsType {
   createdAt: string;
   image: string;
@@ -13,19 +13,18 @@ interface ICardsType {
 }
 const ImageGallerySlider = () => {
   const [current, setCurrent] = useState(0); // Current index
-  const [cards,setCards] = useState<ICardsType[]> ([])
-  const [runUseEffectAgain,setRunUseEffectAgain] = useState(false)
+  const [cards, setCards] = useState<ICardsType[]>([]);
+  const [runUseEffectAgain, setRunUseEffectAgain] = useState(false);
   // https://portfolio-abdulmalik-server.vercel.app
-  useEffect(()=>{
-    fetchApi(`/api/project/allProjects`,"GET",{}).then((data)=>{
-      setCards(data.allProjects)
-    })
-    setRunUseEffectAgain(true)
-
-  },[])
-  useEffect(()=>{
-    setRunUseEffectAgain(false)
-  },[])
+  useEffect(() => {
+    fetchApi(`/api/project/allProjects`, "GET", {}).then((data) => {
+      setCards(data.allProjects);
+    });
+    setRunUseEffectAgain(true);
+  }, []);
+  useEffect(() => {
+    setRunUseEffectAgain(false);
+  }, []);
   const nextCard = () => {
     setCurrent(current === cards.length - 1 ? 0 : current + 1);
   };
@@ -34,28 +33,39 @@ const ImageGallerySlider = () => {
     setCurrent(current === 0 ? cards.length - 1 : current - 1);
   };
   return (
-    <div className='flex flex-col  items-center justify-center' id="portfolio" >
-      <h2 className='uppercase text-4xl font-bold mb-4' >my portfolio</h2>
+    <div className="flex flex-col  items-center justify-center" id="portfolio">
+      <h2 className="uppercase text-4xl font-bold mb-4">my portfolio</h2>
       <div className="flex flex-wrap md:flex-nowrap  justify-center gap-6  md:flex-row flex-col items-center">
-        <button onClick={prevCard}><FaAnglesLeft  size="40px" /></button>
-    {
-        cards.length != 0 &&  Array.from({ length: 3 }, (_, i) => (current + i) % cards.length).map(index => (
-          <>
-          <div key={index} className="relative  rounded-2xl h-full">
-            <Image width="400" height="400"  src={`/api/files/${cards[index].image}`}  alt={cards[index].title} className='rounded-2xl' />
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-200 rounded-2xl">
-              <h2 className="text-white">{cards[index].title}</h2>
-            </div>
-            
-          </div>
-          <p className='mt-4 font-bold text-center' >{cards[index].title}</p>
-          </>
-        ))
-    }
-        <button onClick={nextCard}><FaAnglesRight size="40px" /></button>
+        <button onClick={prevCard}>
+          <FaAnglesLeft size="40px" />
+        </button>
+        {cards.length != 0 &&
+          Array.from({ length: 3 }, (_, i) => (current + i) % cards.length).map(
+            (index) => (
+              <div key={index}>
+                <div className="relative  rounded-2xl h-full">
+                  <Image
+                    width="400"
+                    height="400"
+                    src={`/api/files/${cards[index].image}`}
+                    alt={cards[index].title}
+                    className="rounded-2xl"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-200 rounded-2xl">
+                    <h2 className="text-white">{cards[index].title}</h2>
+                  </div>
+                </div>
+                <p className="mt-4 font-bold text-center">
+                  {cards[index].title}
+                </p>
+              </div>
+            )
+          )}
+        <button onClick={nextCard}>
+          <FaAnglesRight size="40px" />
+        </button>
       </div>
     </div>
   );
 };
 export default ImageGallerySlider;
-
