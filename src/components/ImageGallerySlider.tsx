f47b9f383af2c91235606b8d7423f22d92f3cd6a
abdulmalishakir2001 <abdulmalikshakir2001@gmail.com>
2024-01-30 19:50:40 +0500

    show 3 images on desktop

--- src/components/ImageGallerySlider.tsx
+++ src/components/ImageGallerySlider.tsx
@@ -17,7 +17,7 @@ interface ICardsType {
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 1,
+    items: 3,
     slidesToSlide: 1 // optional, default to 1.
   },
   tablet: {
