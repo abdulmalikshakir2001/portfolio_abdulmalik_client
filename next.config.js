1ddc5a879502db115dd712891bf0f3cfbeb45aaf
abdulmalishakir2001 <abdulmalikshakir2001@gmail.com>
2024-01-30 19:42:02 +0500

    fix image gallery slider layout shifting

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
         protocol: "https",
         hostname: "www.pexels.com",
       },
+      
       {
         protocol: "https",
         hostname: "images.unsplash.com",
